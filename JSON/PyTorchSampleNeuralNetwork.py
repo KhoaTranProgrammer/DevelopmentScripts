905f84cc75e5ce07b83f57aba78bb09ffb477581
KhoaTranProgrammer <khoatranprogrammer@gmail.com>
2024-11-03 21:27:26 +0700

    Combine Nw/Train/Test in 1 python file

--- JSON/PyTorchSampleNeuralNetwork.py
+++ JSON/PyTorchSampleNeuralNetwork.py
@@ -35,3 +35,69 @@ class PyTorchSampleNeuralNetwork():
             
         network = Classifier()
         return network
+
+    def trainNetwork(self, model, trainLoader, validationLoader, criterion, optimizer, epochs):
+        steps = 0
+        running_loss = 0
+        print_every = 200
+        train_losses, test_losses = [], []
+        accuracies = []
+
+        for epoch in range(epochs):
+            for inputs, labels in trainLoader:
+                steps += 1
+                # Move input and label tensors to the default device
+                # inputs, labels = inputs.to(device), labels.to(device)
+                
+                logps = model.forward(inputs)
+                loss = criterion(logps, labels)
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+                
+                if steps % print_every == 0:
+                    test_loss = 0
+                    accuracy = 0
+                    model.eval()
+                    with torch.no_grad():
+                        for inputs, labels in validationLoader:
+                            logps = model.forward(inputs)
+                            batch_loss = criterion(logps, labels)
+                            
+                            test_loss += batch_loss.item()
+                            
+                            # Calculate accuracy
+                            ps = torch.exp(logps)
+                            top_p, top_class = ps.topk(1, dim=1)
+                            equals = top_class == labels.view(*top_class.shape)
+                            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                    print(f"Epoch {epoch+1}/{epochs}.. "
+                        f"Train loss: {running_loss/print_every:.3f}.. "
+                        f"Test loss: {test_loss/len(validationLoader):.3f}.. "
+                        f"Test accuracy: {accuracy/len(validationLoader):.3f}")
+
+                    train_losses.append(running_loss/print_every)
+                    test_losses.append(test_loss/len(validationLoader))
+                    accuracies.append(accuracy/len(validationLoader))
+                    running_loss = 0
+                    model.train()
+
+    def testingNetwork(selfmodel, model, validationLoader):
+        accuracy = 0
+        model.eval()
+        with torch.no_grad():
+            for images, labels in validationLoader:
+                # images = images.to(device)
+                # labels = labels.to(device)
+                logps = model.forward(images)
+
+                ps = torch.exp(logps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equality = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equality.type(torch.FloatTensor)).item()
+        model.train()
+        print(f"Test accuracy: {accuracy/len(validationLoader):.4%}")
